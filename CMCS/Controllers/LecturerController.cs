a158064d7ccbd5e51656097941ce7204799bfc4a
shiss <shiss@ANCHORNOIR>
2024-09-10 16:09:08 +0200

    Add project files.

---
+++ CMCS/Controllers/LecturerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CMCS.Models;
+
+namespace CMCS.Controllers
+{
+    public class LecturerController : Controller
+    {
+        public IActionResult SubmitClaim()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitClaim(LecturerClaimViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save claim logic here (for future development)
+                return RedirectToAction("Success");
+            }
+            return View(model);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ CMCS/Models/LecturerClaimViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CMCS.Models
+{
+    public class LecturerClaimViewModel
+    {
+        public string LecturerName { get; set; }
+        public double HoursWorked { get; set; }
+        public double HourlyRate { get; set; }
+        public string SupportingDocumentPath { get; set; }
+    }
+}
